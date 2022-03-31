2c8831a1125a6d3b116e59205f690d388a929e9c
ApostPap <ApostPap@users.noreply.github.com>
2022-03-31 20:27:47 +0300

    Updated links

--- src/components/contact_left/index.js
+++ src/components/contact_left/index.js
@@ -10,10 +10,10 @@ function Contact_Left() {
               <Col xl={12}>
               <h1 className="p-heading1">Get In Touch</h1>
               <p className="p-heading2">
-              Hola <strong>Visitor !!</strong><br />
-              Thank you for bumping in. If you have any queries/ideas/projects, feel free to <strong>PING</strong> me.
-              I am always open for discussion. You can contact me via the Contact Form <strong>OR</strong> simply click on the social
-              media icon if you are more comfortable there.
+              <strong>Hi There !!</strong><br />
+              Thank you for joining me. Let me know if you have any awesome ideas/projects that we can build together.
+              Feel free to <strong>PING</strong> me. You can contact me via the Contact Form <strong>OR</strong> simply click on the social
+              media icons. Take care.
               </p>
               </Col>
            </Row>
