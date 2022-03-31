57228c256322278d4dfcdb0f074090aebbb9192d
ApostPap <ApostPap@users.noreply.github.com>
2022-03-31 20:53:10 +0300

    Updated route

--- src/nav/index.js
+++ src/nav/index.js
@@ -1,10 +1,6 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "../pages/home";
 import About from "../pages/about";
 import Contact from "../pages/contact";
@@ -12,17 +8,17 @@ import Loading from "../pages/loading";
 import Projects from "../pages/projects";
 
 export default function Nav() {
-  return (
-    <Router basename="/portfolio">
-     <Switch>
-        <Route exact path="/" component={Loading} />
-        <Route path="/home" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/projects" component={Projects} />
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" component={Loading} />
+				<Route path="/home" component={Home} />
+				<Route path="/about" component={About} />
+				<Route path="/contact" component={Contact} />
+				<Route path="/projects" component={Projects} />
 
-        <Route path="*" component={Home} />
-      </Switch>
-    </Router>
-  );
+				<Route path="*" component={Home} />
+			</Switch>
+		</Router>
+	);
 }
